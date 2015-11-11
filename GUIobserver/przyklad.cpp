83fda5ed825e26afe4f24efa2669354aea24917b
marcinbuczko <marcinbuczko@o2.pl>
2015-11-11 20:46:05 +0100

    przyklad komunikacji
    przyklad oparty na -jezyk_c++_mechanizmy_wzorce_biblioteki-robert_nowak

--- GUIobserver/przyklad.cpp
+++ GUIobserver/przyklad.cpp
@@ -2,28 +2,51 @@ typedef signal <void (int)> Signal ;
 typedef boost :: function <void (int)> Slot;
 
 
-class Temperature { // Reprezentuje stan
-	public:
-		void connect (Slot slot) { signal_.connect (slot); }
-		void setTemperature(int temp ) { // Metoda generująca zmianę stanu
-			temp_ = temp;
-			signal_ (temp); // Emisja sygnału
-		}
-private:
-	Signal signal_ ; // Obiekt sygnału
-	int temp_; // Stan, temperatura
-};
 
-
-
-class DisplayCelsius { // Obserwuje stan
-	public:
-	DisplayCelsius() {}
-	void update (int temp ); // Odświeża widok
+class ZPR_Signal {
+	void connect (Slot slot) { signal_.connect (slot); }
+	void setPosition(Status status){
+		status_ = status;
+		signal_(status);
+	}
 	private:
-	// Składowe związane z widokiem
+		Signal signal_ ; // Obiekt sygnału	
+		Status status_;  // Stan
+	
+}
+
+class ZPR_Slot { // Obserwuje stan
+	public:
+	virtual void update (Status status) = 0; // Odświeża widok
 };
 
-Temperature temperature ;
-DisplayCelsius display1 ;
-temperature.connect ( boost :: bind(& DisplayCelsius::update ,&display1 , _1) );
+
+
+
+class Body public: ZPR_Signal{
+	
+};
+
+class Sensor public: ZPR_Signal public: ZPR_Slot{
+	
+};
+ 
+class Kalman public: ZPR_Signal public: ZPR_Slot{
+	
+};
+
+class GUI public: ZPR_Slot{
+	
+};
+ 
+
+Body body;
+Sensor sensor;
+Kalman sensor;
+GUI gui;
+
+
+body.connect( boost :: bind(& Sensor::update ,&sensor , _1) );
+body.connect( boost :: bind(& GUI::update ,&gui , _1) );
+sensor.connect( boost :: bind(& Kalman::update ,&kalman , _1) );
+kalman.connect( boost :: bind(& GUI::update ,&gui , _1) )
