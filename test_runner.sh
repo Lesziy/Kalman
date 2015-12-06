4fa569fab266ca98cb2d8d9bdf4063387737e74c
jpalczewski <jpalczewski@gmail.com>
2015-12-06 15:29:05 +0100

    troche poprawiona wersja runnera

--- test_runner.sh
+++ test_runner.sh
@@ -4,10 +4,13 @@ ARGS=''
 
 if [[ $user ==  'jenkins' ]] 
 then
-	ARGS='--log_format=XML --log_sink=results.xml --log_level=all --report_level=no --result_code=no'
-fi 
-
-for i in ./test-*
-do
-      $i $ARGS
-done
+    for i in ./test-*
+    do
+      $i "--log_format=XML --log_level=all --report_level=no --result_code=no --log_sink=$i.xml"
+    done
+else
+    for i in ./test-*
+    do
+      $i
+    done
+fi
