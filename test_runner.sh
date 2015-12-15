c1e625c873e3ca65e873dccddbbde6459f653464
jpalczewski <jpalczewski@gmail.com>
2015-12-15 03:04:49 +0100

    literowka w test-runnerze

--- test_runner.sh
+++ test_runner.sh
@@ -4,7 +4,7 @@ ARGS=''
 
 if [[ $user ==  'jenkins' ]]
 then
-    for i in $(find .build/test/test-* -type f -executable)
+    for i in $(find ./build/test/test-* -type f -executable)
     do
       $i "--log_format=XML --log_level=all --report_level=no --result_code=no --log_sink=$i.xml"
     done
