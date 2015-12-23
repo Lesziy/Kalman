b08554d11386c250e0b30b408a7eb93824888098
jpalczewski <jpalczewski@gmail.com>
2015-12-23 03:56:58 +0100

    poprawa błędów

--- test_runner.sh
+++ test_runner.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 ARGS=''
-cd ${0%/*}
+
 
 if [[ $user ==  'jenkins' ]]
 then
+    cd /var/lib/jenkins/jobs/Kalman/workspace
     for i in $(find ./build/test/test-* -type f -executable)
     do
       $i "--log_format=XML --log_level=all --report_level=no --result_code=no --log_sink=$i.xml"
