b1e7c95b4b74efa5a06995512e2610a47deac333
jpalczewski <jpalczewski@gmail.com>
2016-01-11 02:10:55 +0100

    parę dodatkowych komentarzy do doxygena

--- src/Common/Observee.h
+++ src/Common/Observee.h
@@ -17,6 +17,7 @@ namespace CommonUtil {
     class Observee
     {
     public:
+        /** \todo GCC wymagało jawnych konstruktorów: czy żadne dodatkowe rzeczy nie będą wymagane pod MSVC? */
         Observee() {};
         ~Observee() {};
         //! Łączy sloty.
