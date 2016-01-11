b2e7f6283f7675017b055dc8024530cda7885966
jpalczewski <jpalczewski@gmail.com>
2016-01-11 01:59:13 +0100

    teoretycznie działający następny test

--- test/test_Common.cpp
+++ test/test_Common.cpp
@@ -1,28 +1,16 @@
 #define BOOST_TEST_MODULE Common_tests
 #include <boost/test/included/unit_test.hpp>
 #include <functional>
-#include <chrono>
-#include <thread>
-#include <vector>
 #include "Common.h"
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/bind/placeholders.hpp>
+
 using namespace boost::unit_test;
 using namespace CommonUtil;
+
 
 class Ping : public Observee
 {
 public:
-    Ping()
-    {
-
-    }
-    ~Ping()
-    {
-
-    }
+    Ping() { }
 
     void setPosition(Status s)
     {
@@ -31,26 +19,20 @@ public:
     }
 };
 
-
 class Pong : public Observator
 {
 
 public:
-    std::vector<int> data;
-    int ok;
-    Status ret;
-    void other() {BOOST_LOG_TRIVIAL(trace) << "other"; ok = -500;}
-    Pong() : ok(1)
-    {
-    };
+    bool ok;
+    Pong() : ok(false) { };
 
     void update(Status s)
     {
-        other();
-        BOOST_LOG_TRIVIAL(trace) << "Pong update, x:" << s.x <<", y:" << s.y;
-        data.push_back(1);
+        if(s.x == 123.0)
+            ok = true;
     }
 };
+
 
 BOOST_AUTO_TEST_SUITE(Common_tests)
 BOOST_AUTO_TEST_CASE(test_true)
@@ -67,12 +49,8 @@ BOOST_AUTO_TEST_CASE(classicUse)
     Ping ping;
     Pong pong;
 
-    ping.connect(std::bind(&Pong::update, pong, std::placeholders::_1));
+    ping.connect(std::bind(&Pong::update, std::ref(pong), std::placeholders::_1));
     ping.setPosition(Status(123.0, 0));
-
-
-
-    BOOST_LOG_TRIVIAL(debug) << "pong.ok:" << pong.data.at(0);
 
     BOOST_REQUIRE(pong.ok);
 }
