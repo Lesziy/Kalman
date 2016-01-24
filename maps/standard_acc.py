ad66cbc824705201dfd59ab305a7322332b085f6
marcinbuczko <marcinbuczko@o2.pl>
2016-01-24 18:21:11 +0100

    działający skrypt z przyśpieszeniami

--- maps/standard_acc.py
+++ maps/standard_acc.py
@@ -1,16 +1,18 @@
 time = 1
-vx, vy = 0 , 0;
-x,y = 0 , 0;
-acc_x, acc_y = 0.1, 0.1;
-noice = 0.1;
+vx, vy = 0 , 0
+x,y = 0 , 0
+acc_x, acc_y = 0.1, 0.1
+noice = 0.1
 import random,math
 def standard_acc():
-    global time, vx, vy, x, y, acc_x, acc_y
-    time = time + 1
+	global time, vx, vy, x, y, acc_x, acc_y
 	ax = acc_x + random.uniform(0, 0.04)
-	ay = acc_y +random.uniform(0, 0.04)
-    x =  x + time * vx + time*time/2 * ax
+	ay = acc_y + random.uniform(0, 0.04)
+	x =  x + time * vx + time*time/2 * ax
 	y =  y + time * vy + time*time/2 * ay	
-    vx = vx + time * ax
+	x = vx + time * ax
 	vy = vy + time * ay
-    return (x,y,ax,ay)
+	time = time + 1
+	return (x,y,ax,ay)
+	
+print standard_acc();
