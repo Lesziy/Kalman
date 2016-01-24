2b29104d71166840b4aa61c829aab58ce0abff83
marcinbuczko <marcinbuczko@o2.pl>
2016-01-24 18:06:38 +0100

    testowy - niedziałający skrypt z przyspieszeniami

---
+++ maps/standard_acc.py
@@ -0,0 +1,16 @@
+time = 1
+vx, vy = 0 , 0;
+x,y = 0 , 0;
+acc_x, acc_y = 0.1, 0.1;
+noice = 0.1;
+import random,math
+def standard_acc():
+    global time, vx, vy, x, y, acc_x, acc_y
+    time = time + 1
+	ax = acc_x + random.uniform(0, 0.04)
+	ay = acc_y +random.uniform(0, 0.04)
+    x =  x + time * vx + time*time/2 * ax
+	y =  y + time * vy + time*time/2 * ay	
+    vx = vx + time * ax
+	vy = vy + time * ay
+    return (x,y,ax,ay)
