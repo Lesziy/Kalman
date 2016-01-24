4e2f29a03974c45d41163592eec932b99ae52b04
marcinbuczko <marcinbuczko@o2.pl>
2016-01-24 18:29:50 +0100

    jeszcze bardziej poprawiony skrypt
    dla ruchu przyspieszonego

--- maps/standard_acc.py
+++ maps/standard_acc.py
@@ -1,18 +1,17 @@
 time = 1
 vx, vy = 0 , 0
 x,y = 0 , 0
-acc_x, acc_y = 0.1, 0.1
-noice = 0.1
+acc_x, acc_y = 1, 0
+noice = 0
 import random,math
 def standard_acc():
 	global time, vx, vy, x, y, acc_x, acc_y
-	ax = acc_x + random.uniform(0, 0.04)
-	ay = acc_y + random.uniform(0, 0.04)
+	ax = acc_x + random.uniform(0, noice)
+	ay = acc_y + random.uniform(0, noice)
 	x =  x + time * vx + time*time/2 * ax
 	y =  y + time * vy + time*time/2 * ay	
 	x = vx + time * ax
 	vy = vy + time * ay
 	time = time + 1
 	return (x,y,ax,ay)
-	
-print standard_acc();
+
