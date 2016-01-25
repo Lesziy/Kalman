865b29728e5758e032a79963cb32fcb4f308a4dc
jpalczewski <jpalczewski@gmail.com>
2016-01-25 20:42:25 +0100

    koniec z dodatkowym plikiem bat [*]

--- after_install.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=%PATH%;C:\local\boost_1_59_0\lib64-msvc-14.0;D:\deps\wxWidgets-3.0.2\lib\vc_x64_lib;D:\deps\Boost\lib64-msvc-14.0;C:\wxWidgets-3.0.2\lib\vc_x64_lib;C:\wxWidgets-3.0.2\lib\vc_x64_lib;D:\deps\SDL\lib\x64;C:\Python27\libs;C:\SDL2-2.0.3\lib\x64;C:\SDL2-2.0.3\lib\x64;C:\boost2\lib64-msvc-14.0;D:\deps\Python27\libs;C:\Python27\libs
-echo Sciezki zostaly zaaktualizowane!
