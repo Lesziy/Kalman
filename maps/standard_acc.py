c3baff7520b179881463cac1834986176cf59c54
marcinbuczko <marcinbuczko@o2.pl>
2016-01-25 02:58:18 +0100

    Działający filtr po wstępnych testach
    po zapuszczeniu standard_acc.py można zobaczyć efekt - wykres z excela w
    doc\wykres_ruch_jednostajnie_przyspieszony.pdf

--- maps/standard_acc.py
+++ maps/standard_acc.py
@@ -1,4 +1,5 @@
 time = 0
+time_step = 0.1
 vx, vy = 0 , 0
 x,y = 0 , 0
 acc_x, acc_y = 1, 0
@@ -8,10 +9,10 @@ def standard_acc():
 	global time, vx, vy, x, y, acc_x, acc_y, noice
 	ax = acc_x + random.uniform(-1 * noice, noice)
 	ay = acc_y
-	x =  x + time * vx + time*time/2 * ax
-	y =  y + time * vy + time*time/2 * ay	
-	vx = vx + time * ax
-	vy = vy + time * ay
-	time = time + 0.1
+	x =  x + time_step * vx + time_step*time_step/2 * ax
+	y =  y + time_step * vy + time_step*time_step/2 * ay	
+	vx = vx + time_step * ax
+	vy = vy + time_step * ay
+	time = time + time_step
 	return (x,y,acc_x,acc_y)
 
--- src/Sensors/SensorWorker.cpp
+++ src/Sensors/SensorWorker.cpp
@@ -5,7 +5,8 @@
 SensorWorker::SensorWorker(const std::pair<double, double>& noiseXY_)
 {
 	srand(time(0));
-	noiseXY = noiseXY;
+	noiseXY = noiseXY_;
+	std::cout << noiseXY.first << " " << noiseXY.second << std::endl;
 }
 
 
@@ -15,10 +16,10 @@ SensorWorker::~SensorWorker()
 
 Status SensorWorker::ThreadProc(Status s) {
 	s.type = SENSORS;
-
+	//std::cout << s.y << " ";
 	s.x = s.x + fRand(-noiseXY.first, noiseXY.first);
 	s.y = s.y + fRand(-noiseXY.second, noiseXY.second);
-
+	//std::cout << s.y << std::endl;
 	return s;
 }
 
