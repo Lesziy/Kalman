9d6e1d5accf14691c081ef2bcec411049fa8afca
marcinbuczko <marcinbuczko@o2.pl>
2016-01-26 03:52:37 +0100

    skrypt do symulacji zachowania robota

--- maps/robo_acc.py
+++ maps/robo_acc.py
@@ -1,24 +1,72 @@
 time = 0
 time_step = 0.1
-vx, vy = 5 , 0
-x,y = 0 , 0
+vx, vy = 2 , 2
+x,y = 10 , 0
 acc_x, acc_y = 0, 0
-noice = 1
-radius = 10
+noice = 2
+radius = 5
 v_static = 5
+x_max = 50
+y_max = 50
 import random,math
-def circle_acc():
-	global time, vx, vy, x, y, acc_x, acc_y, noice, radius
-	turn()
+
+class Move:
+	Left = 1
+	Right = 2
+	Stright = 3
+
+status = Move.Stright
+phase_time = 1
+max_phase_time = 4
+
+def robo_acc():
+	global time, vx, vy, x, y, acc_x, acc_y, noice, radius, status, phase_time, max_phase_time
+	acc_x = 0
+	acc_y = 0
+	if phase_time <= 0:
+		status = random.randint(1, 3)
+		phase_time = random.uniform(max_phase_time/2, max_phase_time)
+		#print status
+
+	if status != Move.Stright:
+		turn()
+	#turn()
 	hamowanie()
+	
 	ax = acc_x + random.uniform(-1 * noice, noice)
 	ay = acc_y + random.uniform(-1 * noice, noice)
 	x =  x + time_step * vx + time_step*time_step/2 * ax
 	y =  y + time_step * vy + time_step*time_step/2 * ay	
 	vx = vx + time_step * ax
 	vy = vy + time_step * ay
+	
+
+	odbicie()
 	time = time + time_step
+	phase_time = phase_time - time_step
+	#print x, y
 	return (x,y,acc_x,acc_y)
+	
+def odbicie():
+	global x, y, x_max, y_max, vx, vy
+	if (x < 0):
+		x = 0
+		if(vx <= 0):
+			vx = -vx
+	elif (x > x_max):
+		vx = -vx
+		if(vx >= 0):
+			x = x_max
+			
+	if (y < 0):
+		y = 0
+		if(vy <= 0):
+			vy = -vy
+	elif (y > y_max):
+		vy = -vy
+		if(vy >= 0):
+			y = y_max
+	
 
 def hamowanie():
 
@@ -39,10 +87,10 @@ def hamowanie():
 			
 		acc_x = acc_x - dvx/time_step;
 		acc_y = acc_y - dvy/time_step;
-	
-	
+
+			
 def turn( ):
-	global time, vx, vy, x, y, acc_x, acc_y, radius
+	global time, vx, vy, x, y, acc_x, acc_y, noice, radius
 	#print vx, vy, acc_x, acc_y
 	A = (vx**2 + vy**2)/radius
 	if vy == 0:
@@ -56,3 +104,9 @@ def turn( ):
 		if vy > 0:
 			acc_x = -1 * acc_x
 		acc_y = -tn_alpha * acc_x
+	if status == Move.Right :
+		acc_x = -acc_x
+		acc_y = -acc_y
+
+#for i in range(0,20):
+	#robo_acc()
