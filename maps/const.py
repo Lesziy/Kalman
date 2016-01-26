19a5033cee79beffe53f56e07c080b46a26b06d1
marcinbuczko <marcinbuczko@o2.pl>
2016-01-26 04:19:09 +0100

    ok

--- maps/const.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python
-time = 1
-import random
-def const():
-   return (0.5,0.5) 
-    
-
-    
- 
--- src/Measurements/Measurements.cpp
+++
@@ -1,9 +0,0 @@
-#include "Measurements.h"
-
-Measurements::Measurements() 
-{
-}
-
-Measurements::~Measurements()
-{
-}
