a9d99c2a1b11f7f7260f8de624ecedc7bbac7ce8
Lesziy <przemyslawsaramonowicz@o2.pl>
2016-01-26 16:03:00 +0100

    zmiana w .bat i w skrypcie do matlaba

--- matlabScripts/script.m
+++ matlabScripts/script.m
@@ -1,3 +1,5 @@
+function [ret] = script(points)
+graphics_toolkit("gnuplot");
 data = dlmread('output.csv', ';');
 data(1,:) = [];
 Gx = data(:,1);
@@ -19,23 +21,23 @@ std_dev_x_sen_gen = std(dx_sen_gen)
 std_dev_y_sen_gen = std(dy_sen_gen)
 %plots
 figure()
-plot([1:100], Sx(1:100,:),"k",   [1:100], Gx(1:100,:), "r","linewidth", 2,    [1:100], Kx(1:100,:),"b","linewidth", 2)
+plot([1:points], Sx(1:points,:),"k",   [1:points], Gx(1:points,:), "r","linewidth", 2,    [1:points], Kx(1:points,:),"b","linewidth", 2)
 legend('{\fontsize{12} generator}','{\fontsize{12} kalman}','{\fontsize{12} sensor}')
 title("plot x","fontsize", 16)
 xlabel("sample","fontsize", 12)
 ylabel("value","fontsize", 12)
 %wykres y
 figure()
-plot([1:100], Sy(1:100,:),"k",[1:100], Gy(1:100,:), "r","linewidth", 2,    [1:100], Ky(1:100,:),"b","linewidth", 2)
+plot([1:points], Sy(1:points,:),"k",[1:points], Gy(1:points,:), "r","linewidth", 2,    [1:points], Ky(1:points,:),"b","linewidth", 2)
 legend('{\fontsize{12} generator}','{\fontsize{12} kalman}','{\fontsize{12} sensor}')
 title("plot y","fontsize", 16)
 xlabel("sample","fontsize", 12)
 ylabel("value","fontsize", 12)
 figure()
-plot(Sx, Sy,"k",   Gx, Gy, "r","linewidth", 2,    Kx, Ky,"b","linewidth", 2)
+plot(Sx(1:points,:), Sy(1:points,:),"k",   Gx(1:points,:), Gy(1:points,:), "r","linewidth", 2,    Kx(1:points,:), Ky(1:points,:),"b","linewidth", 2)
 legend('{\fontsize{12} generator}','{\fontsize{12} kalman}','{\fontsize{12} sensor}')
 title("path","fontsize", 16)
 xlabel("X","fontsize", 12)
 ylabel("Y","fontsize", 12)
-
-pause()
+pause();
+end;
